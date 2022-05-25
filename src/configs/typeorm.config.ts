c9136e57bb58ae326105635befd755f01e043218
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-25 08:48:40 -0300

    refactor: change names os env config in typeorm config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,16 +1,16 @@
 import { ConnectionOptions } from 'typeorm';
 import { environment, isTesting } from '@/consts/envs';
 const databases = {
-  development: process.env.POSTGRES_DATABASE || 'backend_pg_db',
-  test: 'backend_pg_db_test',
+  development: process.env.POSTGRES_DB || 'nodefinance',
+  test: 'nodefinance',
 };
 const typeormConfig: ConnectionOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
   port: process.env.POSTGRES_PORT ? +process.env.POSTGRES_PORT : 5432,
-  username: process.env.POSTGRES_USERNAME,
+  username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE || databases[environment],
+  database: process.env.POSTGRES_DB || databases[environment],
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   dropSchema: isTesting,
   synchronize: isTesting,
