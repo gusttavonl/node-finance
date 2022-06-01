6f3572ebea873674c34af2ba3c2a1dd2ab147981
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-01 09:21:15 -0300

    refactor: remove unecessary files of account

--- src/repositories/account.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Account } from '@/database/entities/account.entity';
-
-@EntityRepository(Account)
-export class AccountRepository extends Repository<Account> { }
